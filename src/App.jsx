87a73a23277e81c4ac867d9af4ab15b8a81407db
watcharin28 <phon567gjo@gmail.com>
2025-03-10 20:38:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>
+    </div>
+  );
+}
+
+export default App;
