c56b3124c05e66ebaefa811ccdf411276b56fe67
pepperpedmak <127705037+pepperpedmak@users.noreply.github.com>
2025-03-10 21:20:52 +0700

    blue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+    <div className="flex items-center justify-center min-h-screen bg-blue-800 text-white">
       <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>
     </div>
   );
