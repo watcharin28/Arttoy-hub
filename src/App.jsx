df70b5d479fbd560bed0e415218301e336c7764b
watcharin28 <phon567gjo@gmail.com>
2025-03-10 21:17:30 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white">
-      <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>
+    <div className="flex items-center justify-center min-h-screen bg-[#0905ff] text-white">
+      <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>\
+      <h1 className="text-3xl font-bold">test</h1>
     </div>
   );
 }
