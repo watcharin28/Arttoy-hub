b5bf7df58920260464299e7c56355ba76565c13c
ZM <zmxidn@gmail.com>
2025-03-23 20:02:36 +0700

    Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import Login from "../component/login";  // Corrected import path, assuming the folder structure
+
+function LoginPage() {
+    return (
+        <>
+            
+            <Login />
+        </>
+    );
+}
+
+export default LoginPage;
