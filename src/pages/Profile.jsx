a35b715d3f07aa71af6c4d21245791e56d7870c1
ZM <zmxidn@gmail.com>
2025-05-29 00:16:41 +0700

    [ UPDATE ] Reorganize file folder //Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import Header from "../component/Buyer/Header";
 import NavBar from "../component/Buyer/navbar";
-import NavbarProfile from "../component/Buyer/Proflie/navbarProfile";
+import NavbarProfile from "../component/Buyer/Profile/navbarProfile";
 const ProfilePage = () => {
     return (
       <div>
