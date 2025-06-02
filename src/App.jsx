c2cf3e114f55a155eca0766da32617425fc5886b
ZM <zmxidn@gmail.com>
2025-06-02 23:41:24 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import SellerRegisterPage from './pages/SellerRegister';
 import SellerHomePage from './pages/Seller';
 import ProductDetailPage from './pages/ProductDetail';
 import CheckOutPage from './pages/CheckOut';
+import CartPage from './pages/Cart';
+import SellerStoreDetailPage from './pages/SellerStore';
 
 function App() {
     return (
@@ -19,9 +21,10 @@ function App() {
                 <Route path="/profile" element={<ProfilePage/>} />
                 <Route path="/sellerRegister" element={<SellerRegisterPage/>} />
                 <Route path="/seller/*" element={<SellerHomePage/>} />
-                <Route path="/product/:product_id" element={<ProductDetailPage />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} />
                 <Route path="/checkout" element={<CheckOutPage />} />
-
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/storeDetail/:storeId" element={<SellerStoreDetailPage />} />
             </Routes>
         </div>
     );
@@ -29,3 +32,4 @@ function App() {
 }
 
 export default App;
+
