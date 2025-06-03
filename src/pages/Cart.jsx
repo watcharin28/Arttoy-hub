c5a32a35cd14b25a75f8d36adc3fce0c0802cbcc
watcharin28 <phon567gjo@gmail.com>
2025-06-03 18:14:07 +0700

    editcomponent

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import Header from "../component/Buyer/Header";
 import NavBar from "../component/Buyer/navbar";
-import Cart from "../component/Buyer/Cart";
+import Cart from "../component/Buyer/cart";
 const CartPage = () => {
     return (
       <div>
